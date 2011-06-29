4d04125c3ce5ca8cf1a0d753fc2058decb40fe14
franklin <franklin@weareinteractive.com>
2011-06-29 19:52:21 +0200

    added license note

--- lib/Foomo/Theme/Frontend/Controller.php
+++ lib/Foomo/Theme/Frontend/Controller.php
@@ -1,9 +1,28 @@
 <?php
 
+/*
+ * This file is part of the foomo Opensource Framework.
+ *
+ * The foomo Opensource Framework is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public License as
+ * published  by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * The foomo Opensource Framework is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * the foomo Opensource Framework. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 namespace Foomo\Theme\Frontend;
 
 /**
- *
+ * @link www.foomo.org
+ * @license www.gnu.org/licenses/lgpl.txt
+ * @author franklin <franklin@weareinteractive.com>
  */
 class Controller
 {
